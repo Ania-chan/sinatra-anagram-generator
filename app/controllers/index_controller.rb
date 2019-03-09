e85cc6ece07f9bbb71e1e479f00f6fdffd961a9a
Ania-chan <ania.jurago@gmail.com>
2019-03-09 21:15:07 +0900

    added error message for words longer than 3 letters

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,5 +10,36 @@ end
 
 post '/' do
     @word = params[:word]
-    redirect "/anagrams/#{@word}"
+    if valid_input?(@word)
+        redirect "/anagrams/#{@word}"
+    else
+        @error = "Please input a word thats 3 letters or less"
+        erb :index
+    end
+end
+
+def three_letters?(input)
+    if input.length <= 3 
+        true
+    else 
+        false
+    end
+end
+
+def distinct_letters?(input)
+    letter_array = input.chars
+    unique_letters = letter_array.uniq
+    if letter_array.length > unique_letters.length
+        false
+    else
+        true
+    end
+end
+
+def valid_input?(input)
+    if three_letters?(input) && distinct_letters?(input)
+        true
+    else
+        false
+    end
 end
