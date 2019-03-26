420ac905986939b93e74e38002efb06f7653b509
Ania-chan <ania.jurago@gmail.com>
2019-03-26 21:05:20 +0900

    fixed word validation

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -12,7 +12,7 @@ end
 post '/' do
     @word = params[:word]
     begin
-        valid_input?(@word)
+        # valid_input?(@word)
         redirect "/anagrams/#{@word}"
     rescue Exception => error
         @error = error.message
@@ -20,28 +20,28 @@ post '/' do
     end
 end
 
-def three_letters?(input)
-    if input.length <= 3 
-        true
-    else 
-        false
-    end
-end
+# def three_letters?(input)
+#     if input.length <= 3 
+#         true
+#     else 
+#         false
+#     end
+# end
 
-def distinct_letters?(input)
-    letter_array = input.chars
-    unique_letters = letter_array.uniq
-    if letter_array.length > unique_letters.length
-        false
-    else
-        true
-    end
-end
+# def distinct_letters?(input)
+#     letter_array = input.chars
+#     unique_letters = letter_array.uniq
+#     if letter_array.length > unique_letters.length
+#         false
+#     else
+#         true
+#     end
+# end
 
-def valid_input?(input)
-    if distinct_letters?(input)
-        true
-    else
-        raise Exception.new("Word must be less than or equal to 3 characters.")
-    end
-end
+# def valid_input?(input)
+#     if distinct_letters?(input)
+#         true
+#     else
+#         raise Exception.new("Word must be less than or equal to 3 characters.")
+#     end
+# end
